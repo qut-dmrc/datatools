17296195bada3a922afae2d108e05642b45008f7
nic <nic@suzor.com>
2022-07-01 07:02:08 +1000

    removed bq_dest paramater

--- datatools/gcloud.py
+++ datatools/gcloud.py
@@ -183,8 +183,8 @@ class GCloud:
         # Emergency save routine. We should be able to find some way of dumping the data.
         # Try multiple methods in order until we get a result.
         try:
-            if 'schema' in params and params['schema']:
-                params['destination'] = params.get('destination', params.get('bq_dest'))
+            if 'schema' in params and 'destination' in params:
+                destination = params['destination']
                 self.upload_rows(rows=data, **params)
                 logger.info(f"Uploaded data to BigQuery: {destination}.")
                 return destination
