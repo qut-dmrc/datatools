6e94d70c121db4788c9062bd1acc4c5296a94a8e
Nic Suzor <nic@suzor.com>
2022-07-25 16:53:36 +1000

    yelp business review scraper erady for testing

--- datatools/datasets/yelp.py
+++ datatools/datasets/yelp.py
@@ -8,3 +8,13 @@ def BlackOwnedBusinesses():
     df = pd.read_json('gs://dmrc-platforms/yelp/Black_owned_businesses.json', orient='records', lines=True)
     df['link'] = 'https://yelp.com' + df['link']
     return df
+
+def BlackOwnedBusinessesReviews():
+    ### This dataset was scraped from yelp in July 2022
+    # URL: 'https://www.yelp.com/collection/FXDOTfw9y7LhSA_VZbpAmg/Black-Owned-Businesses-Collection'
+
+    try:
+        df = pd.read_json('gs://dmrc-platforms/yelp/Black_owned_businesses_reviews.json', orient='records', lines=True)
+    except IOError:
+        return pd.DataFrame(columns=['biz_id', 'link'])
+    return df
